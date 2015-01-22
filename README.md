6ea06e430fec7b19b4acdbaf72ccd9e375206e38
Tenet <qaman88@gmail.com>
2015-01-22 10:47:00 +0200

    Revert "New Web Page BitBucket Clone"
    This reverts commit 8848037c05a5ec6b90ed3cd300a5f1be84760113.

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# README #
+qaman88.github.io
+=================
 
-Run index.html
-
-### What is this repository for? ###
-University of Cape Town, Computer Science Nightmare Webpage
-Version v1.0.5
-
-### How do I get set up? ###
-private
-
-### Contribution guidelines ###
-contact
-
-### Who do I talk to? ###
-me
+My Static Web Page Based On CSS, HTML and Javascript
--- javascripts/contentLoader.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $("#header").load("content/headerSurface.html");
-    $("#footer").load("content/footerSurface.html");
-    $("#contentProject").load("content/projectContent.html");
-    $("#contentAbout").load("content/aboutContent.html");
-    $("#contentComSci").load("content/comSciContent.html");
-    $("#contentIndex").load("content/indexContent.html");
-    $("#contentElecEng").load("content/elecEngContent.html");
-});
